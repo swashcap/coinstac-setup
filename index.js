4eb79933da931529d754517ae80f093fd9986e3b
Cory Reed <swashcap@gmail.com>
2016-08-04 11:44:51 -0700

    Add consortium name and user count options.

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ const funnyVersions = require('funny-versions');
 const pkg = require('./package.json');
 const program = require('commander');
 
+const DEFAULT_USER_COUNT = 3;
+
+/**
+ * Parse the `usercount` option.
+ *
+ * @param {string} value
+ * @returns {number}
+ */
+function userCount(value) {
+  const parsed = parseInt(value, 10);
+
+  return Number.isNaN(parsed) || parsed < 0 ? DEFAULT_USER_COUNT : parsed;
+}
+
 program
   .version(pkg.version)
   .option('-c, --computation [value]', 'Computation', 'bisect')
+  .option('--consortium [value]', 'Consortium name')
   .option('-d, --database [value]', 'Database URL', 'http://localhost:5984')
   .option('-u, --username [value]', 'Username', 'demo1')
+  .option('--usercount [value]', 'User count', userCount, DEFAULT_USER_COUNT)
   .parse(process.argv);
 
 Promise.all([
@@ -34,15 +50,20 @@ Promise.all([
       throw new Error('Couldn\'t find ridge regression');
     }
 
-    const name = funnyVersions.generate();
+    const name = program.consortium || funnyVersions.generate();
     const id = name.replace(/\W/g, '').toLowerCase();
+    const users = [program.username];
+
+    for (var i = 1, il = program.usercount; i < il; i++) {
+      users.push(`demo${i + 1}`);
+    }
 
     return axios.put(`${program.database}/consortia/${id}`, {
       activeComputationId: compResponse.data._id,
       description: 'This is a sample consortium!',
       label: name,
       tags: [],
-      users: [program.username, 'demo2', 'demo3'],
+      users,
       owners: [program.username]
     });
   })
