bd9a74103a07e25a78d99cd205208634ef0ca4a7
Cory Reed <swashcap@gmail.com>
2016-08-12 11:35:41 -0700

    Add consortium and user count flags.

--- index.js
+++ index.js
@@ -5,27 +5,50 @@ const funnyVersions = require('funny-versions');
 const pkg = require('./package.json');
 const program = require('commander');
 
+const DEFAULT_CONSORTIUM_COUNT = 1;
 const DEFAULT_USER_COUNT = 3;
 
 /**
- * Parse the `usercount` option.
+ * Get a number parser with a default.
  *
  * @param {string} value
- * @returns {number}
+ * @returns {Function}
  */
-function userCount(value) {
-  const parsed = parseInt(value, 10);
+function getNumberParser(defaultValue) {
+  return function numberParser(value) {
+    const parsed = parseInt(value, 10);
 
-  return Number.isNaN(parsed) || parsed < 0 ? DEFAULT_USER_COUNT : parsed;
+    return Number.isNaN(parsed) || parsed < 0 ? defaultValue : parsed;
+  };
+}
+
+/**
+ * Get an ID from a name.
+ *
+ * @param {string} name
+ * @returns {string}
+ */
+function getId(name) {
+  return name.replace(/\W/g, '').toLowerCase();
 }
 
 program
   .version(pkg.version)
   .option('-c, --computation [value]', 'Computation', 'bisect')
   .option('--consortium [value]', 'Consortium name')
+  .option(
+    '--consortiumcount [value]', 'Consortium count',
+    getNumberParser(DEFAULT_CONSORTIUM_COUNT),
+    DEFAULT_CONSORTIUM_COUNT
+  )
   .option('-d, --database [value]', 'Database URL', 'http://localhost:5984')
   .option('-u, --username [value]', 'Username', 'demo1')
-  .option('--usercount [value]', 'User count', userCount, DEFAULT_USER_COUNT)
+  .option(
+    '--usercount [value]',
+    'User count',
+    getNumberParser(DEFAULT_USER_COUNT),
+    DEFAULT_USER_COUNT
+  )
   .parse(process.argv);
 
 Promise.all([
@@ -47,25 +70,32 @@ Promise.all([
     });
 
     if (!compResponse) {
-      throw new Error('Couldn\'t find ridge regression');
+      throw new Error(`Couldn't find computation ${program.computation}`);
     }
 
-    const name = program.consortium || funnyVersions.generate();
-    const id = name.replace(/\W/g, '').toLowerCase();
+    const names = [program.consortium || funnyVersions.generate()];
     const users = [program.username];
 
-    for (var i = 1, il = program.usercount; i < il; i++) {
+    for (let i = 1, il = program.usercount; i < il; i++) {
       users.push(`demo${i + 1}`);
     }
+    for (let i = 1, il = program.consortiumcount; i < il; i++) {
+      names.push(funnyVersions.generate());
+    }
 
-    return axios.put(`${program.database}/consortia/${id}`, {
-      activeComputationId: compResponse.data._id,
-      description: 'This is a sample consortium!',
-      label: name,
-      tags: [],
-      users,
-      owners: [program.username]
-    });
+    return Promise.all(names.map(name => {
+      const id = getId(name);
+
+      return axios.put(`${program.database}/consortia/${id}`, {
+        activeComputationId: compResponse.data._id,
+        activeComputationInputs: [],
+        description: 'This is a sample consortium!',
+        label: name,
+        tags: [],
+        users,
+        owners: [program.username]
+      });
+    }));
   })
   .then(() => console.log('Consortia set up'))
   .catch(error => console.error(error));
