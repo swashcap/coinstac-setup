a3deec19b2544589c2f59121d51371425c0fb246
Cory Reed <swashcap@gmail.com>
2016-09-14 12:30:50 -0700

    Refactor to contain an export.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const funnyVersions = require('funny-versions');
 const pkg = require('./package.json');
 const program = require('commander');
 
+const DEFAULT_COMPUTATION = 'ridge';
 const DEFAULT_CONSORTIUM_COUNT = 1;
+const DEFAULT_DATABASE = 'http://localhost:5984';
+const DEFAULT_USERNAME = 'demo1';
 const DEFAULT_USER_COUNT = 3;
 
 /**
@@ -32,70 +35,106 @@ function getId(name) {
   return name.replace(/\W/g, '').toLowerCase();
 }
 
-program
-  .version(pkg.version)
-  .option('-c, --computation [value]', 'Computation', 'bisect')
-  .option('--consortium [value]', 'Consortium name')
-  .option(
-    '--consortiumcount [value]', 'Consortium count',
-    getNumberParser(DEFAULT_CONSORTIUM_COUNT),
-    DEFAULT_CONSORTIUM_COUNT
-  )
-  .option('-d, --database [value]', 'Database URL', 'http://localhost:5984')
-  .option('-u, --username [value]', 'Username', 'demo1')
-  .option(
-    '--usercount [value]',
-    'User count',
-    getNumberParser(DEFAULT_USER_COUNT),
-    DEFAULT_USER_COUNT
-  )
-  .parse(process.argv);
+/**
+ * COINSTAC setup.
+ * @module
+ *
+ * @param {Object} options
+ * @param {string} [options.computation=DEFAULT_COMPUTATION]
+ * @param {string} [options.consortium]
+ * @param {number} [options.consortiumcount=DEFAULT_CONSORTIUM_COUNT]
+ * @param {string} [options.database=DEFAULT_DATABASE]
+ * @param {number} [options.usercount=DEFAULT_USER_COUNT]
+ * @param {string} [options.username=DEFAULT_USERNAME]
+ * @returns {Promise}
+ */
+function coinstacSetup(options) {
+  const computation = options.computation || DEFAULT_COMPUTATION;
+  const consortium = options.consortium;
+  const consortiumcount = options.consortiumcount || DEFAULT_CONSORTIUM_COUNT;
+  const database = options.database || DEFAULT_DATABASE;
+  const usercount = options.usercount || DEFAULT_USER_COUNT;
+  const username = options.username || DEFAULT_USERNAME;
 
-Promise.all([
-  axios.get(`${program.database}/consortia/_all_docs`),
-  axios.get(`${program.database}/computations/_all_docs`),
-])
-  .then(([consortiaResponse, computationsResponse]) => {
-    if (consortiaResponse.data.rows.length) {
-      throw new Error('Consortia database already populated');
-    }
+  return Promise.all([
+    axios.get(`${database}/consortia/_all_docs`),
+    axios.get(`${database}/computations/_all_docs`),
+  ])
+    .then(([consortiaResponse, computationsResponse]) => {
+      if (consortiaResponse.data.rows.length) {
+        throw new Error('Consortia database already populated');
+      }
 
-    return Promise.all(computationsResponse.data.rows.map(row => {
-      return axios.get(`${program.database}/computations/${row.id}`);
-    }));
-  })
-  .then(responses => {
-    const compResponse = responses.find(r => {
-      return r.data.name.indexOf(program.computation) > -1;
-    });
-
-    if (!compResponse) {
-      throw new Error(`Couldn't find computation ${program.computation}`);
-    }
-
-    const names = [program.consortium || funnyVersions.generate()];
-    const users = [program.username];
-
-    for (let i = 1, il = program.usercount; i < il; i++) {
-      users.push(`demo${i + 1}`);
-    }
-    for (let i = 1, il = program.consortiumcount; i < il; i++) {
-      names.push(funnyVersions.generate());
-    }
-
-    return Promise.all(names.map(name => {
-      const id = getId(name);
-
-      return axios.put(`${program.database}/consortia/${id}`, {
-        activeComputationId: compResponse.data._id,
-        activeComputationInputs: [],
-        description: 'This is a sample consortium!',
-        label: name,
-        tags: [],
-        users,
-        owners: [program.username]
+      return Promise.all(computationsResponse.data.rows.map(row => {
+        return axios.get(`${database}/computations/${row.id}`);
+      }));
+    })
+    .then(responses => {
+      const compResponse = responses.find(r => {
+        return r.data.name.indexOf(computation) > -1;
       });
-    }));
-  })
-  .then(() => console.log('Consortia set up'))
-  .catch(error => console.error(error));
+
+      if (!compResponse) {
+        throw new Error(`Couldn't find computation ${program.computation}`);
+      }
+
+      const names = [consortium || funnyVersions.generate()];
+      const users = [username];
+
+      for (let i = 1, il = usercount; i < il; i++) {
+        users.push(`demo${i + 1}`);
+      }
+      for (let i = 1, il = consortiumcount; i < il; i++) {
+        names.push(funnyVersions.generate());
+      }
+
+      return Promise.all(names.map(name => {
+        const id = getId(name);
+
+        return axios.put(`${database}/consortia/${id}`, {
+          activeComputationId: compResponse.data._id,
+          activeComputationInputs: [[[
+            'Right-Hippocampus',
+            'TotalGrayVol',
+          ]]],
+          description: 'This is a sample consortium!',
+          label: name,
+          tags: [],
+          users,
+          owners: [username],
+        });
+      }));
+    })
+    .then(() => console.log('Consortia set up'))
+    .catch(error => console.error(error));
+}
+
+/**
+ * Called directly.
+ * {@link http://stackoverflow.com/a/6398335}
+ */
+if (require.main === module) {
+  program
+    .version(pkg.version)
+    .option('-c, --computation [value]', 'Computation', DEFAULT_COMPUTATION)
+    .option('--consortium [value]', 'Consortium name')
+    .option(
+      '--consortiumcount [value]', 'Consortium count',
+      getNumberParser(DEFAULT_CONSORTIUM_COUNT),
+      DEFAULT_CONSORTIUM_COUNT
+    )
+    .option('-d, --database [value]', 'Database URL', DEFAULT_DATABASE)
+    .option('-u, --username [value]', 'Username', DEFAULT_USERNAME)
+    .option(
+      '--usercount [value]',
+      'User count',
+      getNumberParser(DEFAULT_USER_COUNT),
+      DEFAULT_USER_COUNT
+    )
+    .parse(process.argv);
+
+  coinstacSetup(program);
+}
+
+module.exports = coinstacSetup;
+
